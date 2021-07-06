506ad59d3bf707a89af1ac7fce2c38c5101c9a13
RamonVillalba <damianarevalos@hotmail.com>
2021-07-05 21:57:35 -0300

    feat: Global style added

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import { Router } from 'react-router-dom';
 import Routes from './routes';
 import history from './services/history';
 
+// Gloalb styles
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => (
   <Router history={history}>
     <Routes />
+    <GlobalStyle />
   </Router>
 );
 
